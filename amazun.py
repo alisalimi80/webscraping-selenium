b8cb2c2b40fcb380d7180d717ea62ade5661a5b7
alisalimi80 <96053924+alisalimi80@users.noreply.github.com>
2022-04-06 19:29:52 +0430

    Update amazun.py

--- amazun.py
+++ amazun.py
@@ -4,7 +4,7 @@ import csv
 
 browser = webdriver.Chrome()
 browser.get("https://www.amazon.ae/s?k=tablet+samsung&sprefix=tablet+sa%2Caps%2C347&ref=nb_sb_ss_ts-doa-p_1_9")
-namelist=[]
+
 price = browser.find_elements(By.CLASS_NAME, "a-price-whole")
 names = browser.find_elements_by_css_selector("h2.s-line-clamp-4>a>span")
 result=[["id","name","price"]]
